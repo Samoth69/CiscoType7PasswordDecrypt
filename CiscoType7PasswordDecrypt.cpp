e34b90833a579b340655b4ab059143c7a52868dd
Samoth <thomasviolent@gmail.com>
2020-09-08 14:21:13 +0200

    Improving script

--- CiscoType7PasswordDecrypt.cpp
+++ CiscoType7PasswordDecrypt.cpp
@@ -13,23 +13,23 @@ void decryptPW(string *in, string *out) {
 	//mdp hashé
 	string pwHashed = in->substr(2);
 
-	//on réserve la variable de sortie avec le bon nombre de caractère
+	//on réserve la variable de sortie avec le bon nombre de caractères
 	out->reserve(pwHashed.size() / 2);
 
-	int counter = 0;
-
 	for (int i = 0; i < pwHashed.size() / 2; i++) {
+		//index du caractère en traitement
 		int cur_index = (i + index) % 53;
 
+		//on récupère le caractère actuel dans le salt et le mdp
 		char cur_salt = salt[cur_index];
-		char cur_hex = stoi(pwHashed.substr(counter, 2),0,16);
+		char cur_hex = stoi(pwHashed.substr(i * 2, 2),0,16);
 
 		//ceci est la ligne importante
+		//XOR entre le salt et le hash
 		char clearTextChar = cur_salt ^ cur_hex;
 
+		//on ajoute le caractère décrypter dans la chaine
 		out->push_back(clearTextChar);
-
-		counter+=2;
 	}
 }
 
